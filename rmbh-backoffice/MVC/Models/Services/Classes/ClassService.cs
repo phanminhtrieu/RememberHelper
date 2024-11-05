30c271cefa96f2413f8603471b4613557784f9d6
Trieu <phanminhtrieu04@gmail.com>
2024-11-05 09:30:14 +0700

    41_Finish

--- rmbh-backoffice/MVC/Models/Services/Classes/ClassService.cs
+++ rmbh-backoffice/MVC/Models/Services/Classes/ClassService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using rmbh.Entity;
 using rmbh.Entity.Entities.Manipulation;
+using rmbh.Entity.Enums;
 using rmbh_backoffice.MVC.Models.Dtos.Classes;
 
 namespace rmbh_backoffice.MVC.Models.Services.Classes
@@ -8,6 +10,9 @@ namespace rmbh_backoffice.MVC.Models.Services.Classes
     public interface IClassService
     {
         Task<IEnumerable<ClassDto>> GetAllClassesByUserId(string userId);
+        Task<int> CreateAsync(ClassRequest request);
+        Task<int> UpdateAsync(int classId, ClassRequest request);
+        Task<int> DeleteClass(int classId);
     }
 
     public class ClassService : IClassService
@@ -41,6 +46,68 @@ namespace rmbh_backoffice.MVC.Models.Services.Classes
                 .ToListAsync();
 
             return classes; // Trả về danh sách các ClassDto
+        }
+
+        public async Task<int> CreateAsync(ClassRequest request)
+        {
+            int effectedRow = 0;
+
+            var newClass = new Class
+            {
+                Logo = request.Logo,
+                Title = request.Title,
+                Description = request.Description,
+                Visibility = ClassVisibility.Private,
+                CreatedDate = DateTime.UtcNow,
+                ModifiedDate = DateTime.UtcNow,
+            };
+
+            await _context.Classes.AddAsync(newClass);
+            effectedRow = await _context.SaveChangesAsync(); 
+
+            var userClass = new UserClass
+            {
+                UserId = request.CreatedUserId,
+                ClassId = newClass.Id, 
+                Role = ClassRole.Owner,
+            };
+
+            await _context.UserClasses.AddAsync(userClass);
+            effectedRow += await _context.SaveChangesAsync();
+
+            return effectedRow;
+        }
+
+        public async Task<int> UpdateAsync(int classId, ClassRequest request)
+        {
+            var existingClass = await _context.Classes.FindAsync(classId);
+
+            if (existingClass == null)
+            {
+                throw new Exception("Class not found");
+            }
+
+            existingClass.Title = request.Title;
+            existingClass.Description = request.Description;
+            existingClass.Logo = request.Logo;
+            existingClass.ModifiedDate = DateTime.UtcNow;
+
+            _context.Classes.Update(existingClass);
+
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteClass(int classId)
+        {
+            var existingClass = await _context.Classes.FindAsync(classId);
+
+            if (existingClass == null)
+            {
+                throw new Exception("Class not found");
+            }
+
+            _context.Classes.Remove(existingClass);
+            return await _context.SaveChangesAsync();
         }
     }
 }
--- rmbh.API/Controllers/Frontend/ClassesController.cs
+++ rmbh.API/Controllers/Frontend/ClassesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using rmbh_backoffice.MVC.Models.Dtos.Classes;
 using rmbh_backoffice.MVC.Models.Services.Classes;
 
 namespace rmbh.API.Controllers.Frontend
@@ -33,6 +34,57 @@ namespace rmbh.API.Controllers.Frontend
                 return NotFound(); 
             }
             return Ok(classes);
+        }
+
+        /// <summary>
+        /// Add a new class
+        /// </summary>
+        /// <param name="request">Class creation request data</param>
+        /// <returns>Response indicating the result of the operation</returns>
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAsync([FromBody] ClassRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Invalid class data.");
+            }
+
+            try
+            {
+                await _classService.CreateAsync(request);
+                return Ok("Class created successfully.");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (optional)
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Update class by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody] ClassRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Invalid class data.");
+            }
+
+            try
+            {
+                await _classService.UpdateAsync(id, request);
+                return Ok("Class updated successfully.");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (optional)
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
     }
 }
